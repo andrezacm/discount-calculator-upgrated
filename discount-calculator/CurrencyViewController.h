02177d430ac27ae5f793a0a5e9fa05715c56908a
andrezacm <andreza.cmedeiros@gmail.com>
2015-04-05 20:30:07 -0700

    add Currency view controller with swipe gesture to right

---
+++ discount-calculator/CurrencyViewController.h
@@ -0,0 +1,16 @@
+//
+//  CurrencyViewController.h
+//  discount-calculator
+//
+//  Created by Andreza da Costa Medeiros on 4/5/15.
+//  Copyright (c) 2015 Andreza da Costa Medeiros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CurrencyViewController : UIViewController
+
+//@property(strong, nonatomic) NSDecimalNumber * finalPrice;
+//@property(strong, nonatomic) NSDecimalNumber * originalPrice;
+
+@end

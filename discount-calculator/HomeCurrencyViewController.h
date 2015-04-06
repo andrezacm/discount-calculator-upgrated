aec097cbc1e3d1bfc8c815264672947a657fcd95
andrezacm <andreza.cmedeiros@gmail.com>
2015-04-05 21:28:46 -0700

    Add home currency view controller

---
+++ discount-calculator/HomeCurrencyViewController.h
@@ -0,0 +1,14 @@
+//
+//  HomeCurrencyViewController.h
+//  discount-calculator
+//
+//  Created by Andreza da Costa Medeiros on 4/5/15.
+//  Copyright (c) 2015 Andreza da Costa Medeiros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeCurrencyViewController : UIViewController
+
+
+@end
---
+++ discount-calculator/HomeCurrencyViewController.m
@@ -0,0 +1,25 @@
+//
+//  HomeCurrencyViewController.m
+//  discount-calculator
+//
+//  Created by Andreza da Costa Medeiros on 4/5/15.
+//  Copyright (c) 2015 Andreza da Costa Medeiros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HomeCurrencyViewController.h"
+
+@implementation HomeCurrencyViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+  
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end

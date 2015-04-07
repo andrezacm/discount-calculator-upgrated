68bbda84f50faa221a995e97b7400cf393e76f0a
andrezacm <andreza.cmedeiros@gmail.com>
2015-04-07 11:15:34 -0700

    Add exchange view controller

---
+++ discount-calculator/ExchangeViewController.h
@@ -0,0 +1,16 @@
+//
+//  ExchangeViewController.h
+//  discount-calculator
+//
+//  Created by Andreza da Costa Medeiros on 4/7/15.
+//  Copyright (c) 2015 Andreza da Costa Medeiros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExchangeViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *homeCurrencyValue;
+@property (weak, nonatomic) IBOutlet UILabel *foreignCurrencyValue;
+
+@end
